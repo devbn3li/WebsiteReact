24349d51922d0709427e26e17657a18b16856f83
DarkCode662432 <ff9371668@gmail.com>
2024-01-30 20:56:55 +0200

    init router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Import React and required modules from react-router
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Import page components
+import App from './App';
+import LandingPage from './pages/LandingPage';
+// import NotFound from './Pages/NotFound'; // Import the 404 page component
+
+// Import style sheets
+import './index.css';
+
+
+export function Application() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<LandingPage />} />
+            {/* <Route path="*" element={<NotFound />} />  */}
+          </Route>
+        </Routes>
+    </Router> 
+  );
+}
+
+const rootContainer = document.getElementById('root');
+ReactDOM.createRoot(rootContainer).render(<Application />);
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Hiiiiiiiiiiiiiiiii</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
