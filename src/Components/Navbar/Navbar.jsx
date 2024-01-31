58e7e95de1c3b2304acffd2d2ed5bba8eb64bd07
DarkCode662432 <ff9371668@gmail.com>
2024-01-31 18:34:50 +0200

    [enhancement]

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import NavLink from './NavbarLink';
 import PrimaryBtn from '../Buttons/PrimaryBtn';
-import { Link } from 'react-router-dom';
 
-const links = [
+// Define navigation links
+const NAV_LINKS = [
   { path: '/about', title: 'About us' },
   { path: '/contact', title: 'Contact us' },
-]
+];
 
 const Navbar = () => {
+  // State for mobile navigation drawer
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {setIsOpen(!isOpen)};
+  // Function to toggle state of mobile navigation drawer
+  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <nav className='flex justify-between items-center px-[5%] max-w-[1400px] box-border w-screen bg-white h-[4.5rem] font-inter m-auto fixed top-0 left-1/2 -translate-x-1/2 z-50'>
       <div className='bg-white z-50 max-[991px]:w-full max-[991px]:flex max-[991px]:h-full justify-between items-center'>
-        <Link to='/' className="flex gap-[6px]">
+        <Link to='/'  className="flex gap-[6px]">
           <img className='aspect-square w-[52px]' src="/src/assets/images/logo.png" alt="LevelUp logo" />
           <span className="text-[35px] font-bold inline-block text-[#333]">LevelUp</span>
         </Link>
@@ -26,16 +29,15 @@ const Navbar = () => {
           <span className={`w-6 h-[2px] bg-[#121212] transition-all duration-[.45s] ${isOpen?'rotate-45 -translate-y-2 delay-[.35s]':'rotate-0 translate-y-0'} `}></span>
         </button>
       </div>
-
       <div className={`flex gap-4 font-bold text-center transition-all duration-[.45s] box-border ${isOpen ? 'top-full' : '-top-[100vh]'} left-0 z-40 
                       max-[991px]:flex-col max-[991px]:absolute max-[991px]:w-full max-[991px]:px-[5%] max-w-full max-[991px]:pb-[6.5rem] max-[991px]:h-[calc(100vh-4.5rem)] max-[991px]:text-lg
                       `}>
         <div className='flex max-[991px]:flex-col'>
-          {links.map((page, index) => (
-            <NavLink key={index} path={page.path} title={page.title} />
+          {NAV_LINKS.map((link, index) => (
+            <NavLink key={index} path={link.path} title={link.title} />
           ))}
         </div>
-        <PrimaryBtn path='#' title='Make an impact' />
+        <PrimaryBtn path='#' title='Make an Impact' />
       </div>
     </nav>
   );
--- src/Components/Navbar/NavbarLink.jsx
+++ src/Components/Navbar/NavbarLink.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const NavbarLink = ({path, title}) => {
+// NavbarLink component. Each link element in the navbar
+const NavbarLink = ({ path, title }) => {
+  // Tailwind CSS classes for the Link component
+  const linkClasses = 'px-4 py-2 hover:bg-[#e6e6e5] rounded-lg transition-all duration-[.45s] inline-block max-[991px]:hover:bg-transparent max-[991px]:px-0 max-[991px]:py-3';
+
   return (
-    <Link to={path} className='px-4 py-2 hover:bg-[#e6e6e5] rounded-lg transition-all duration-[.45s] inline-block 
-                                max-[991px]:hover:bg-transparent max-[991px]:px-0 max-[991px]:py-3
-                                '>
+    <Link to={path} className={linkClasses}>
       {title}
     </Link>
   );
 }
 
+// PropTypes for props typechecking
 NavbarLink.propTypes = {
   path: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired
