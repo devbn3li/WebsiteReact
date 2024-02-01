ba411f019d26d85fd090421714e23b97deb9fec4
DarkCode662432 <ff9371668@gmail.com>
2024-02-01 03:06:20 +0200

    [fix]

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -17,8 +17,8 @@ const Navbar = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className='flex justify-between items-center px-[5%] max-w-[1400px] box-border w-screen bg-white h-[4.5rem] font-inter m-auto fixed top-0 left-1/2 -translate-x-1/2 z-50'>
-      <div className='bg-white z-50 max-[991px]:w-full max-[991px]:flex max-[991px]:h-full justify-between items-center'>
+    <nav className='flex justify-between items-center px-[5%] max-[991px]:px-0 max-w-[1400px] box-border w-screen bg-white h-[4.5rem] font-inter m-auto fixed top-0 left-1/2 -translate-x-1/2 z-50'>
+      <div className='bg-white z-50 max-[991px]:w-full max-[991px]:flex max-[991px]:p-[5%] max-[991px]:h-full justify-between items-center'>
         <Link to='/'  className="flex gap-[6px]">
           <img className='aspect-square w-[52px]' src="/src/assets/images/logo.png" alt="LevelUp logo" />
           <span className="text-[35px] font-bold inline-block text-[#333]">LevelUp</span>
@@ -29,8 +29,8 @@ const Navbar = () => {
           <span className={`w-6 h-[2px] bg-[#121212] transition-all duration-[.45s] ${isOpen?'rotate-45 -translate-y-2 delay-[.35s]':'rotate-0 translate-y-0'} `}></span>
         </button>
       </div>
-      <div className={`flex gap-4 font-bold text-center transition-all duration-[.45s] box-border ${isOpen ? 'top-full' : '-top-[100vh]'} left-0 z-40 
-                      max-[991px]:flex-col max-[991px]:absolute max-[991px]:w-full max-[991px]:px-[5%] max-w-full max-[991px]:pb-[6.5rem] max-[991px]:h-[calc(100vh-4.5rem)] max-[991px]:text-lg
+      <div className={`flex gap-4 font-bold text-center transition-all duration-[.45s] box-border ${isOpen ? 'top-full' : '-top-[100vh]'} left-0 z-40 bg-white
+                      max-[991px]:flex-col max-[991px]:absolute max-[991px]:w-full max-[991px]:px-[5%] max-w-full max-[991px]:pb-[6.5rem] max-[991px]:h-[calc(100vh-4.5rem)] max-[991px]:text-lg max-[991px]:border-b max-[991px]:border-black
                       `}>
         <div className='flex max-[991px]:flex-col'>
           {NAV_LINKS.map((link, index) => (
