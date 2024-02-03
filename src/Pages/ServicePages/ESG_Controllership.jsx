550756057516a560048efbc669f5cc51861815fb
devbn3li <fudwhc@gmail.com>
2024-02-03 14:20:39 +0200

    fix services icons

--- src/Pages/ServicePages/ESG_Controllership.jsx
+++ src/Pages/ServicePages/ESG_Controllership.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { FaAngular } from "react-icons/fa6";
+import { FaLeaf } from "react-icons/fa";
 import SliderLogos from '../../Components/SliderLogos/SliderLogos';
 import Cursal from '../../Components/Cursal/Cursal';
 import Hero_Services from '../../Components/Hero_Services';
-import Footer from '../../Components/Footer'
 
 
 const services = [
   {
     title: "ESG Controller Role Establishment",
     description: "Our service guides you through the meticulous process of establishing the ESG Controller role.",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "ESG Controller Role Establishment",
       discription: "Our service guides you through the meticulous process of establishing the ESG Controller role. Beginning with crafting the initial job description, we ensure alignment with leading practices and tailor it to your organization's specific needs. By carefully defining the responsibilities and expectations, we lay the foundation for a strategic and impactful ESG leadership position.",
@@ -20,7 +19,7 @@ const services = [
   {
     title: "ESG Governance Work",
     description: "Navigate the intricacies of ESG governance seamlessly with our comprehensive support.",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "ESG Governance Work",
       discription: "Navigate the intricacies of ESG governance seamlessly with our comprehensive support. From the inception of the ESG Controller role to the implementation of effective governance practices, we provide end-to-end assistance. This includes equipping the role with essential documents and tools, such as an ESG compliance strategy and methodology manual. Our service ensures a well-rounded governance approach that aligns with industry standards and regulatory expectations.",
@@ -30,7 +29,7 @@ const services = [
   {
     title: "Leading Practices in ESG Controllership",
     description: "Empower your ESG Controller to embody industry-leading practices with our specialized service. ",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "Leading Practices in ESG Controllership",
       discription: "Empower your ESG Controller to embody industry-leading practices with our specialized service. We go beyond establishing the role, guiding them in understanding and navigating new governance frameworks such as the Sustainability COSO framework, The EU Corporate Sustainability Due Diligence Directive (CSDDD), and FCA/anti-greenwashing regulations. This comprehensive approach ensures your ESG Controller stays at the forefront of evolving industry standards.",
@@ -40,7 +39,7 @@ const services = [
   {
     title: "ESG Initiative Management",
     description: "Our service guarantees a holistic and effective approach to managing environmental, social, and governance initiatives.",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "ESG Initiative Management",
       discription: "Our service guarantees a holistic and effective approach to managing environmental, social, and governance initiatives. From identifying and managing qualitative risks and controls associated with ESG to providing a detailed compliance checklist, we ensure that your organization's initiatives align with best practices. This service is designed to enhance the overall effectiveness and impact of your ESG endeavors.+main differences lie in the focus and scope of the ESG controllership efforts, and we are committed to providing a service that is tailored to your organization's specific needs.",
--- src/Pages/ServicePages/ESG_IA_program.jsx
+++ src/Pages/ServicePages/ESG_IA_program.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { FaAngular } from "react-icons/fa6";
 import SliderLogos from '../../Components/SliderLogos/SliderLogos';
 import Cursal from '../../Components/Cursal/Cursal';
 import Hero_Services from '../../Components/Hero_Services';
-import Footer from '../../Components/Footer'
+import { FaLeaf } from "react-icons/fa";
 
 
 const services = [
   {
     title: "ESG Internal Audit (IA) Strategy Development",
     description: "Our service guides you in meticulously planning and scoping your ESG audits, whether conducted separately or integrated. ",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "ESG Internal Audit (IA) Strategy Development",
       discription: "Our service guides you in meticulously planning and scoping your ESG audits, whether conducted separately or integrated. Our strategies aim for consistent usage and standards, encompassing quantitative metrics, industry standards, and other compliance areas. We ensure that your audits align with the latest sustainability practices, providing a robust foundation for comprehensive ESG evaluation.",
@@ -20,7 +19,7 @@ const services = [
   {
     title: "ESG IA Methodology Manual",
     description: "Navigate the evolving ESG landscape confidently with our comprehensive ESG IA Methodology Manual.",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "ESG IA Methodology Manual",
       discription: "Navigate the evolving ESG landscape confidently with our comprehensive ESG IA Methodology Manual. This detailed guide will outline the methodology for conducting ESG audits, addressing the nuances of new governance frameworks such as the Sustainability COSO framework, The EU Corporate Sustainability Due Diligence Directive (CSDDD), and FCA/anti-greenwashing regulations. It serves as a valuable resource for understanding and implementing best practices in ESG auditing.",
@@ -30,7 +29,7 @@ const services = [
   {
     title: "Qualitative Risks and Controls",
     description: "Identifying and managing qualitative risks and controls related to ESG is critical for a thorough audit. ",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "Qualitative Risks and Controls",
       discription: "Identifying and managing qualitative risks and controls related to ESG is critical for a thorough audit. We provide expertise in this area, assisting you in navigating the complex landscape of qualitative considerations associated with Environmental, Social, and Governance factors. This ensures a comprehensive approach to risk management within the ESG context.",
@@ -40,7 +39,7 @@ const services = [
   {
     title: "Requirements Tracking Program",
     description: "Our detailed ESG Internal Audit Checklist ensures comprehensive scope coverage.",
-    icon: <FaAngular />,
+    icon: <FaLeaf />,
     data: {
       title: "Requirements Tracking Program",
       discription: "Our detailed ESG Internal Audit Checklist ensures comprehensive scope coverage. Tailored to address the unique aspects of ESG audits, this checklist guarantees that all relevant areas are thoroughly audited, promoting effective evaluation and risk management in line with industry standards and regulatory requirements.",
