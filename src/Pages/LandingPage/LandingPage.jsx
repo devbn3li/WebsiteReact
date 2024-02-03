53f8963ca0282d4d58dd0847d7741831e7ed6e86
sondos <so_saad@yahoo.com>
2024-02-03 21:34:20 +0200

    create-popup

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from '../../Components/Hero/Hero';
 import Benefits from '../../Components/Benefits/Benefits';
 import Offer from '../../Components/Offer/Offer';
 import Testimonials from '../../Components/Testimonials/Testimonials';
 import Why from '../../Components/Why';
 import FAQs from '../../Components/FAQs';
+import Popup from '../../Components/Popup/Popup';
 
-const LandingPage = () => {
+const LandingPage = () => {  
+    const [isModalOpen, setModalOpen] = useState(false);
+    const closeModal = () => {
+      setModalOpen(false);
+    };
+  
+    useEffect(() => {
+      // Open the modal after the component mounts
+      setModalOpen(true);
+    }, []); // Empty dependency array ensures the effect runs only once
   return (
     <div className='flex flex-col justify-center items-center w-full font-inter text-[#121212]'>
       <Hero />
+      <Popup isOpen={isModalOpen} closeModal={closeModal} />
       <Benefits />
       <Offer />
       <Testimonials />
