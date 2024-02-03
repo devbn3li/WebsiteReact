7aaa3a0fb4465d0ca3a96cc06f92983740d6bb82
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-02-03 14:15:12 +0200

    Update Academy page and add new link to Navbar

--- src/Components/Acamedy/Mentors.jsx
+++ src/Components/Acamedy/Mentors.jsx
@@ -8,10 +8,10 @@ function Mentors() {
       <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>
     </a>
     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
-     Learn from the best with LevelUp Academy
+    Explore ESG Insights at LevelUp Academy
       </h1>
     <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">
-    Social responsibility is the cornerstone of economic</p>
+    Dive into the world of Environmental, Social, and Governance (ESG) with LevelUp Academy&lsquo;s dedicated page.    </p>
     {/* <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
       <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
         Learn more
