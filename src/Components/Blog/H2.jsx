82a20bbab7b5155c2234a084e10d5ed060d9de15
DarkCode662432 <ff9371668@gmail.com>
2024-02-03 21:13:31 +0200

    Blog templete

---
+++ src/Components/Blog/H2.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const H2 = ({ text }) => {
+  return (
+    <h1 className='text-[40xpx]'>
+      {text}
+    </h1>
+  );
+}
+
+H2.propTypes = {
+  text: PropTypes.string.isRequire
+}
+
+export default H2;
