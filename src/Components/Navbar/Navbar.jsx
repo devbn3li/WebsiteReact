7404916d691e601f026c9e7c84adfe2af13c7264
DarkCode662432 <ff9371668@gmail.com>
2024-02-04 02:59:23 +0200

    fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,34 +3,19 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import NavLink from "./NavbarLink";
 import PrimaryBtn from "../Buttons/PrimaryBtn";
-
+import { FaLeaf } from 'react-icons/fa6';
+import { IoEarthSharp } from 'react-icons/io5';
+ 
 
 // Define navigation links
 const NAV_LINKS = [
   { path: "/academy", title: "Academy" },
-
   { path: "/about", title: "About us" },
-
   { path: "/contact", title: "Contact us" },
 ];
 
-// {
-//   title: "Services",
-//   dropdownItems: [
-//     { path: "/service/esg-controllership", title: "Controllership " },
-//     { path: "/service/esg-ia-program", title: "IA-program " },
-//     { path: "/service/esg-governance", title: "Governance " },
-//     { path: "/service/esg-assurance", title: "Assurance " },
-//     { path: "/service/strategic-stakeholder", title: "Strategic " },
-//     { path: "/service/esg-strategy", title: "Strategy " },
-//     { path: "/service/esg-compliance", title: "Compliance " },
-//     { path: "/service/erm-program", title: "Program" },
-//     { path: "/service/esg-diagnostic", title: "Diagnostic " },
-//     { path: "/service/board-level", title: "Board" },
-//   ],
-// },  
 
-const dropdownItems = [
+const services = [
   { path: "/service/esg-controllership", title: "Controllership " },
   { path: "/service/esg-ia-program", title: "IA-program " },
   { path: "/service/esg-governance", title: "Governance " },
@@ -41,24 +26,24 @@ const dropdownItems = [
   { path: "/service/erm-program", title: "Program" },
   { path: "/service/esg-diagnostic", title: "Diagnostic " },
   { path: "/service/board-level", title: "Board" },
-]
+];
 
 const Navbar = () => {
   // State for mobile navigation drawer
   const [isOpen, setIsOpen] = useState(false);
-  const [dropDown, setDropDown] = useState(false);
+  const [isServicesOpen, setServicesActive] = useState(false);
+  // const [solution, setSolution] = useState(false);
 
   // Function to toggle state of mobile navigation drawer
   const toggleMenu = () => setIsOpen(!isOpen);
-  const toggleDropDown = () => setDropDown(!dropDown);
-
-  // const openDropdown = () => setIsOpen(true);
-  // const closeDropdown = () => setIsOpen(false);
+  
+  // Toggle menu handling functions
+  const toggleServicesActive = () => setServicesActive(!isServicesOpen);
+  // const toggleSolution = () => setSolution(!solution);
 
   return (
     <nav className="bg-white fixed top-0   font-inter m-auto  -translate-x-1/2 z-50 left-1/2 flex justify-center   w-full">
       <div
-        // onClick={closeDropdown}
         className="flex justify-between items-center  px-[5%] max-[991px]:px-0 max-w-[1400px]  box-border w-full bg-white h-[4.5rem]"
       >
         <div className="bg-white z-50 max-[991px]:w-full max-[991px]:flex  max-[991px]:p-[5%] max-[991px]:h-full justify-between items-center">
@@ -100,27 +85,32 @@ const Navbar = () => {
                       max-[991px]:flex-col max-[991px]:absolute max-[991px]:w-full max-[991px]:px-[5%] max-w-full max-[991px]:pb-[6.5rem] max-[991px]:h-[calc(100vh-4.5rem)] max-[991px]:text-lg max-[991px]:border-b max-[991px]:border-black
                       `}
         >
-          <div className="flex max-[991px]:flex-col">
+          <div className="flex max-[991px]:flex-col justify-center items-center">
             <div className="relative group cursor-pointer" >
-              <div onClick={toggleDropDown} className='px-4 py-2 hover:bg-[#e6e6e5] rounded-lg transition-all duration-[.45s] inline-block max-[991px]:hover:bg-transparent max-[991px]:px-0 max-[991px]:py-3'>Services</div>
-              <div className={`absolute w-[250px]  bg-[#03C2AB]  py-2 px-4 rounded-3xl shadow-lg group-hover:block ${dropDown ? 'block' : 'hidden'}`}>
-                {dropdownItems.map((item, index) => (
-                  <Link key={index} to={item.path} onClick={()=>{setDropDown(false)}} className="block text-[#fff] duration-500 hover:text-gray-600 py-1">
+              <div onClick={toggleServicesActive} className='px-4 py-2 hover:bg-[#e6e6e5] rounded-lg transition-all duration-[.45s] inline-block max-[991px]:hover:bg-transparent max-[991px]:px-0 max-[991px]:py-3'>Services</div>
+              <div className={`w-[250px]  bg-[#03C2AB] overflow-hidden flex flex-col justify-center px-4 min-[991px]:shadow-lg max-[991px]:w-screen group-hover:h-[360px] transition-all min-[991px]:rounded-3xl min-[991px]:absolute ${isServicesOpen ? 'h-[360px]' : 'h-0'}`}>
+                {services.map((item, index) => (
+                  <Link key={index} to={item.path} onClick={() => {setServicesActive(false) }} className="block text-[#fff] duration-500 hover:text-gray-600 py-1">
                     {item.title}
                   </Link>
                 ))}
               </div>
             </div>
-            {NAV_LINKS.map((link, index) => (
-              <NavLink
-                key={index}
-                path={link.path}
-                title={link.title}
-                dropdownItems={link.dropdownItems}
-                toggleMenu={toggleMenu}
-              // openDropdown={openDropdown}
-              />
-            ))}
+            <div className="group cursor-pointer" >
+              <div className='px-4 py-2 hover:bg-[#e6e6e5] rounded-lg transition-all duration-[.45s] inline-block max-[991px]:hover:bg-transparent max-[991px]:px-0 max-[991px]:py-3'>Solutions</div>
+              <div className={`text-white w-screen flex justify-center items-center group-hover:translate-y-0 hover:delay-0 delay-100 translate-y-20 left-1/2 -translate-x-1/2 top-full bg-[#02C2AB] transition-all px-4 box-border border-b-[#3e857d] hover:border-b-4 group-hover:h-[150px] h-0 overflow-hidden max-[991px]:flex-col max-[991px]:hover:h-auto max-[991px]:translate-x-0 min-[991px]:absolute gap-1`}>
+                <Link className="h-[130px] max-[991px]:h-[200px] w-1/2 flex justify-center items-center hover:bg-white/10 rounded-xl flex-col gap-2 max-[991px]:w-full"><span className="text-2xl"><FaLeaf /></span><span>Solutions 1</span></Link>
+                <Link className="h-[130px] w-1/2 flex justify-center items-center hover:bg-white/10 rounded-xl flex-col gap-2 max-[991px]:w-full"><span className="text-2xl"><IoEarthSharp /></span><span>Solutions 2</span></Link>
+              </div>
+            </div>
+              {NAV_LINKS.map((link, index) => (
+                <NavLink
+                  key={index}
+                  path={link.path}
+                  title={link.title}
+                  toggleMenu={toggleMenu}
+                />
+              ))}
           </div>
           <PrimaryBtn path="/contact" title="Make an Impact" />
         </div>
