ddf4e1b1d61b166b30dcf4c41cf66e9e4d5991fb
DarkCode662432 <ff9371668@gmail.com>
2024-02-05 06:07:49 +0200

    Navbar V2.

--- src/Components/Navbar/NavbarLink.jsx
+++ src/Components/Navbar/NavbarLink.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 // NavbarLink component. Each link element in the navbar
 const NavbarLink = ({ path, title }) => {
   // Tailwind CSS classes for the Link component
-  const linkClasses = 'px-4 py-2 hover:bg-[#e6e6e5] rounded-lg transition-all duration-[.45s] inline-block max-[991px]:hover:bg-transparent max-[991px]:px-0 max-[991px]:py-3';
+  const linkClasses = 'px-4 py-2 hover:bg-[#e6e6e5] hover:font-bold rounded-lg transition-all duration-[.45s] inline-block max-[991px]:hover:bg-transparent max-[991px]:px-0 max-[991px]:py-3';
   return (
     <Link to={path} className={linkClasses}>
       {title} 
