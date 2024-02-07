9c005440e8e7f0bf8d37d08c348aa6bd61dce475
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-02-07 18:46:59 +0200

    Refactor BenefitsCard and TestimonialCard components

--- src/Components/Testimonials/TestimonialCard.jsx
+++ src/Components/Testimonials/TestimonialCard.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 const TestimonialCard = ({ name, message, title = '' }, index) => {
   return (
     // The main container for the testimonial card with applied styling for appearance and responsiveness.
-    <div key={index} className=' sm:w-[300px] md:w-[350px] lg:w-[450px] xl:w-[550px] h-[300px] sm:h-auto  overflow-x-auto sm:overflow-x-hidden  rounded-2xl bg-[#b3ffd6] p-3 sm:p-5 text-base flex flex-col justify-between text-[#121212]  box-border '>
+    <div key={index} className=' hover:scale-95 duration-700 cursor-pointer select-none sm:w-[300px] md:w-[350px] lg:w-[450px] xl:w-[550px] h-[300px] sm:h-auto  overflow-x-auto sm:overflow-x-hidden  rounded-2xl bg-[#b3ffd6] p-3 sm:p-5 text-base flex flex-col justify-between text-[#121212]  box-border '>
       {/* Displays the message of the testimonial. */}
       <p className='mb-2 sm:mb-8 pr-1 font-medium '>{message}</p>
       {/* Container for the name and title (if present) of the person giving the testimonial. */}
