187410dbf3d02450de057d1e38875cf69dc7cc64
sondos <so_saad@yahoo.com>
2024-02-07 13:45:15 +0200

    Create Purpose Section

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -3,14 +3,17 @@ import AboutComponent from '../../Components/AboutComponent/AboutComponent';
 import Business from '../../Components/Business';
 import Why from '../../Components/Why';
 import Jobs from '../../Components/Jobs';
+import PurposeComponent from '../../Components/AboutComponent/ Purpose';
 
 function About() {
   return (
     <div>
       <AboutComponent />
       <Business />
+      <PurposeComponent/>
       <Why />
       <Jobs />
+      
     </div>
   )
 }
