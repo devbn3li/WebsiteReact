c184dd642621cf1a3d0ae7c3de77ec6cd2d882cf
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-02-07 18:22:04 +0200

    Update image paths in BlogCardsList component

--- src/Components/BlogCard/BlogCardsList.jsx
+++ src/Components/BlogCard/BlogCardsList.jsx
@@ -5,7 +5,7 @@ function BlogCardsList() {
  // Array of blog objects
  const blogs = [
     {
-      img: "../../../public/assets/images/business-people-shaking-hands-together.avif",
+      img: "/assets/images/business-people-shaking-hands-together.avif",
       title: "Empowering for Sustainable Success: Navigating Complexities and Driving Unified Action",
       description:
         " In today's business landscape, the responsibility of driving sustainability initiatives often falls on the CEO. However, the success of these endeavors heavily relies on the collective efforts of the leadership team. To avoid any accusations of Greenwashing, it's crucial for the business leaders to actively participate in monitoring and tracking sustainability goals within their respective teams.",
@@ -15,7 +15,7 @@ function BlogCardsList() {
       link: "/blog/all/Empowering-for-Sustainable-Success",
     },
     {
-      img: "../../../public/assets/images/business-people-shaking-hands-together.avif",
+      img: "/assets/images/business-people-shaking-hands-together.avif",
       title: "Empowering the C Suite for Sustainable Success",
       description:
         " In today's business landscape, the responsibility of driving sustainability initiatives often falls on the CEO. However, the success of these endeavors heavily relies on the collective efforts of the C-suite team. To avoid any accusations of Greenwashing, it's crucial for the C-suite leaders to actively participate in monitoring and tracking sustainability goals within their respective teams.",
@@ -25,7 +25,7 @@ function BlogCardsList() {
       link: "/blog/all/Empowering-the-C-Suite-for-Sustainable-Success",
     },
     {
-      img: "../../../public/assets/images/business-people-shaking-hands-together.avif",
+      img: "/assets/images/business-people-shaking-hands-together.avif",
       title: "Understanding Greenwashing and Its Consequences",
       description:
         " The term greenwashing is used across various industries, but its meaning lacks consistency, creating challenges for companies seeking clarity. For this reason, the three European Supervisory Authorities (ESAs) have offered guidance in their recent call for evidence on greenwashing. This guidance outlines core characteristics associated with greenwashing, such as misleading communications occurring intentionally or unintentionally at the entity, product, or service level. Trying to combine the various definitions proposed, greenwashing can be interpreted as: the act of providing a deceptive or inaccurate portrayal of the environmental advantages of a product, service, or organization.",
