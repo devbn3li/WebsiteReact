212b03d385fd9d81a460813ee051afee33d939f0
DarkCode662432 <ff9371668@gmail.com>
2024-02-07 15:16:29 +0200

    [enhancement]

--- src/Components/Navbar/Dropdown.jsx
+++ src/Components/Navbar/Dropdown.jsx
@@ -18,14 +18,14 @@ const Dropdown = ({ isTabOpen, toggleTab, closeTab, data }) => {
         {/* End Button */}
         
         {/* Start Dropdown */}
-        <div className={`w-screen transition-all flex justify-center items-center min-[1130px]:absolute min-[1130px]:left-1/2 min-[1130px]:top-[calc(100%+1px)] box-border min-[1130px]:-translate-x-1/2 min-[1130px]:shadow-md group-hover:border-b border-b-[#02c2ab] bg-white min-[1130px]:group-hover:h-[320px] ${isTabOpen ? 'min-[1130px]:h-[320px] h-auto max-[1130px]:pt-5' : 'h-0'} overflow-hidden`}>
+        <div className={`w-screen transition-all flex justify-center items-center min-[1130px]:absolute min-[1130px]:left-1/2 min-[1130px]:top-[calc(100%+1px)] box-border min-[1130px]:-translate-x-1/2 min-[1130px]:shadow-md group-hover:border-b border-b-[#02c2ab] bg-white h-0 ${(data.moreLinks1 || data.moreLinks2) ? 'min-[1130px]:group-hover:h-[320px]' : 'min-[1130px]:group-hover:h-[250px]'} ${isTabOpen && ((data.moreLinks1 || data.moreLinks2) ? 'max-[1130px]:h-auto max-[1130px]:py-5': "max-[1130px]:h-auto max-[1130px]:py-5")} overflow-hidden`}>
           {/* Start Inner Container */}
-          <div className='max-w-[1400px] flex-col w-full text-start flex items-center bg-red transition-all px-4 box-border max-[1130px]:flex-col'>
+          <div className='max-w-[1400px] flex-col w-full text-start flex items-center transition-all px-4 box-border max-[1130px]:flex-col'>
 
             {/* Start Highlighted Tabs */}
             <div className='flex w-full gap-2 h-[200px] max-[1130px]:flex-col max-[1130px]:h-auto '>
               {/* Start Tab One */}
-              <Link to="/service/esg" className="w-1/2 group/learn max-[1130px]:w-[90%] max-[1130px]:mx-[5%] box-border hover:shadow-md hover:border-2 transition-all px-12 flex justify-between items-center bg-[#02c2ab]/40 border border-[#02c2ab] rounded-xl gap-2" onClick={closeTab}>
+              <Link to={data.tab1.path} className="w-1/2 group/learn max-[1130px]:w-[90%] max-[1130px]:mx-[5%] box-border hover:shadow-md hover:border-2 transition-all px-12 flex justify-between items-center bg-[#02c2ab]/40 border border-[#02c2ab] rounded-xl gap-2" onClick={closeTab}>
                 <span className='flex flex-col items-start gap-2 pt-4'>
                   <span className='font-bold'>{data.tab1.title}</span>
                   <span className=''>{data.tab1.description}</span>
@@ -36,7 +36,7 @@ const Dropdown = ({ isTabOpen, toggleTab, closeTab, data }) => {
               {/* End Tab One */}
 
               {/* Start Tab Two */}
-              <Link to="/service/business" className="group/learn max-[1130px]:w-[90%] border-dashed hover:border-solid max-[1130px]:mx-[5%] box-border hover:shadow-md px-12 w-1/2 flex group/learn justify-center items-center rounded-xl gap-2 transition-all border border-gray-200 hover:border-[#02c2ab] hover:border-2" onClick={closeTab}>
+              <Link to={data.tab2.path} className="group/learn max-[1130px]:w-[90%] border-dashed hover:border-solid max-[1130px]:mx-[5%] box-border hover:shadow-md px-12 w-1/2 flex group/learn justify-center items-center rounded-xl gap-2 transition-all border border-gray-200 hover:border-[#02c2ab] hover:border-2" onClick={closeTab}>
                 <span className='flex flex-col items-start gap-2 pt-4 '>
                   <span className='font-bold'>{data.tab2.title}</span>
                   <span className=''>{data.tab2.description}</span>
@@ -52,25 +52,25 @@ const Dropdown = ({ isTabOpen, toggleTab, closeTab, data }) => {
             <div className='w-full justify-between flex max-[1130px]:flex-col'>
 
               {/* Other Links 1 */}
-              <div className='flex flex-col w-1/2 max-[1130px]:w-[90%] max-[1130px]:mx-[5%] max-[1130px]:my-5 gap-4'>
+              {data.moreLinks1 && (<div className={`flex flex-col ${data.moreLinks2 && 'w-1/2'} max-[1130px]:w-[90%] max-[1130px]:mx-[5%] max-[1130px]:my-5 gap-4`}>
                 <div className="text-black font-bold text-lg min-[1130px]:hidden">- {data.moreLinks1.title}: </div>
                 <div className="flex flex-wrap">
                   {data.moreLinks1.links.slice(0, 7).map((link, index) => (
                     <SubNavbarLink key={index} title={link.title} Icon={link.Icon} path={link.path} clear={closeTab} />
                   ))}
                 </div>
-              </div>
+              </div>)}
               {/* End Other Links 1 */}
 
               {/* Start Other Links 2 */}
-              <div className='flex flex-wrap w-1/2 max-[1130px]:w-[90%] max-[1130px]:mx-[5%] max-[1130px]:my-5 gap-4'>
+              {data.moreLinks1 && (<div className={`flex flex-col ${data.moreLinks1 && 'w-1/2'} max-[1130px]:w-[90%] max-[1130px]:mx-[5%] max-[1130px]:my-5 gap-4`}>
                 <div className="text-black font-bold text-lg min-[1130px]:hidden"> - {data.moreLinks2.title}: </div>
                 <div className="flex flex-wrap">
                   {data.moreLinks2.links.slice(0, 7).map((link, index) => (
                       <SubNavbarLink title={link.title} Icon={link.Icon} path={link.path} clear={closeTab} key={index} />
                   ))}
                 </div>
-              </div>
+              </div>)}
               {/* End Other Links 2 */}
             </div>
             {/* End Other Links */}
@@ -92,11 +92,13 @@ Dropdown.propTypes = {
     title: PropTypes.string.isRequired,
     tab1: PropTypes.shape({
       title: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
       Icon: PropTypes.object.isRequired
     }),
     tab2: PropTypes.shape({
       title: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
       Icon: PropTypes.object.isRequired,
     }),
