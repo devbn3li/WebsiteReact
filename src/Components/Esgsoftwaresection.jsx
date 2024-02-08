84f064ec040413c70a86dfa78223df3ad5118359
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-02-08 03:23:52 +0200

    Update ESG software section and hero section descriptions

--- src/Components/Esgsoftwaresection.jsx
+++ src/Components/Esgsoftwaresection.jsx
@@ -3,10 +3,7 @@ import PrimaryButton from '../Components/Buttons/PrimaryButton';
 
 // Data for software solutions
 const softwareSolutions = [
-  {
-    name: 'LevelUp ESG Pulse',
-    description: 'An AI-powered, cloud-based tool for rapid assessment of your ESG performance against prominent frameworks, providing actionable insights.'
-  },
+ 
   {
     name: 'Sustainability Tracker',
     description: 'Simplify your ESG data management and reporting with our intuitive software, enabling real-time insights and performance tracking.'
@@ -15,10 +12,7 @@ const softwareSolutions = [
     name: 'LevelUp O2',
     description: 'Automate your carbon accounting and manage your entire value chain emissions efficiently with our AI-powered GHG emissions calculator.'
   },
-  {
-    name: 'LevelUp Data Lab',
-    description: 'Leverage advanced analytics and data science to uncover deep insights, driving strategic decisions for your sustainability goals.'
-  }
+  
 ];
 
 const ESGSoftwareSection = () => {
@@ -28,7 +22,7 @@ const ESGSoftwareSection = () => {
       <div className="container text-[#002E70] mx-auto">
         <h2 className="mb-4 text-4xl text-center max-w-[1200px] font-extrabold tracking-tight leading-none text-[#002E70] md:text-5xl lg:text-6xl ">Enhance Your ESG Performance with Our Software Solutions</h2>
         <div className='mx-auto pt-12 h-full max-w-[1400px] p-2'>
-        <div className="grid grid-cols-1 justify-center gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 justify-center gap-6 md:grid-cols-2 ">
           {softwareSolutions.map((solution, index) => (
             <div key={index} className="p-6 bg-[#02c2ab]/30 hover:bg-[#02c2ab] duration-500 cursor-pointer hover:scale-105 rounded-lg shadow-md">
               <h3 className="text-2xl !font-extrabold text-[#112b65]  mb-4">{solution.name}</h3>
--- src/Components/HeroSoftware.jsx
+++ src/Components/HeroSoftware.jsx
@@ -16,10 +16,10 @@ const HeroSection = () => {
     >
       <div className="container mx-auto px-4 text-center">
         <h1 className="mb-4 text-4xl max-w-[1200px] font-extrabold tracking-tight leading-none  md:text-5xl lg:text-6xl ">
-          ESG Software Solutions
+        sustainability and  ESG Softwares
         </h1>
         <p className="mb-8 text-lg font-normal text-white/80 lg:text-xl  ">
-          Unlock the full potential of your ESG initiatives with our innovative software solutions. From diagnostics to reporting and beyond, LevelUp offers tailored tools such as ESG Pulse, Sustainability Tracker, O2, and Data Lab.
+          Unlock the full potential of your  sustainability and ESG initiatives with our innovative software solutions. From diagnostics to reporting and beyond, LevelUp offers tailored tools such as ESG Pulse, Sustainability Tracker, O2, and Data Lab.
         </p>
         <PrimaryButton path="/contact" title="Request Access" classes="mt-4" />
       </div>
