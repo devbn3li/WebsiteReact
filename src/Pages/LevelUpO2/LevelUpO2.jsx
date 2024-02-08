ef30592e8ea310fa342fc40ecd09884a1821d8d2
devbn3li <fudwhc@gmail.com>
2024-02-08 04:40:39 +0200

    new section LevelUpO2

--- src/Pages/LevelUpO2/LevelUpO2.jsx
+++ src/Pages/LevelUpO2/LevelUpO2.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../../Components/LevelUpO2/Hero';
 import Challenge from '../../Components/LevelUpO2/Challenge';
 import Solutions from '../../Components/LevelUpO2/Solutions';
 import Benefits from '../../Components/LevelUpO2/Benefits.jsx';
+import Call from '../../Components/LevelUpO2/Call.jsx';
 
 
 function LevelUpO2() {
@@ -12,6 +13,7 @@ function LevelUpO2() {
       <Challenge />
       <Solutions />
       <Benefits />
+      <Call />
     </div>
   );
 }
