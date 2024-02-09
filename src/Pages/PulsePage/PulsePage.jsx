404e5a5a84bfe2fb9f4fdf1475e975af79be3c8b
devbn3li <fudwhc@gmail.com>
2024-02-09 08:20:06 +0200

    enhancement puls page

--- src/Pages/PulsePage/PulsePage.jsx
+++ src/Pages/PulsePage/PulsePage.jsx
@@ -1,123 +1,148 @@
-import React from 'react';
+import React from "react";
 import CardData from "../../Components/SolCard/CardData";
-import Futures from '../../Components/PulsePageCompontent/Futures';
+import Futures from "../../Components/PulsePageCompontent/Futures";
 import DS from "../../../public/assets/images/DS.png";
 import { MdCelebration, MdOutlineReport } from "react-icons/md";
 import { FaLevelUpAlt } from "react-icons/fa";
-import { FaBrain, FaUniversalAccess, FaChartLine } from 'react-icons/fa';
+import { FaBrain, FaUniversalAccess, FaChartLine } from "react-icons/fa";
 import { PiStrategy } from "react-icons/pi";
 import backgroundImage from "../../../public/assets/images/bg/1.jpg";
-import Hero_2 from '../../Components/Hero/Hero_2';
-import Benefits from '../../Components/Benefits/Benefits';
-import CTA1 from '../../Components/CTA/CTA_1';
-import CTA2 from '../../Components/CTA/CTA_2';
-
+import Hero_2 from "../../Components/Hero/Hero_2";
+import Benefits from "../../Components/Benefits/Benefits";
+import CTA1 from "../../Components/CTA/CTA_1";
+import CTA2 from "../../Components/CTA/CTA_2";
 
 const PulsePage = () => {
   const heroData = {
     title: "Empower Your Sustainability Journey with LevelUp ESG Pulse",
-    description: "Accelerate your business growth and sustainability impact with AI-powered ESG assessments",
+    description:
+      "Accelerate your business growth and sustainability impact with AI-powered ESG assessments",
     CTATitle: "Learn More",
-    CTAPath: "/contact"
-  }
+    CTAPath: "/contact",
+  };
 
   const benefitsData = {
-    title: 'Core Advantages of LevelUp ESG Pulse',
-    description: 'Harness the power of AI for a strategic, comprehensive approach to sustainability that\'s tailored to your industry and needs:',
+    title: "Core Advantages of LevelUp ESG Pulse",
+    description:
+      "Harness the power of AI for a strategic, comprehensive approach to sustainability that's tailored to your industry and needs:",
     benefits: [
       {
         icon: <FaBrain />,
         title: "AI-Driven Insights",
-        description: "Unlock precise, AI-powered ESG assessments for rapid and accurate insights, enabling smarter sustainability decisions.",
+        description:
+          "Unlock precise, AI-powered ESG assessments for rapid and accurate insights, enabling smarter sustainability decisions.",
       },
       {
         icon: <FaUniversalAccess />,
         title: "Versatile and Tailored",
-        description: "Whether you're in manufacturing, tech, or services, our platform adapts to your sector, delivering customized, actionable guidance.",
+        description:
+          "Whether you're in manufacturing, tech, or services, our platform adapts to your sector, delivering customized, actionable guidance.",
       },
       {
         icon: <FaChartLine />,
         title: "Strategic Benchmarking",
-        description: "Empower your strategy with benchmarking against global standards and competitors, identifying clear paths for improvement and growth.",
-      }
-    ]
+        description:
+          "Empower your strategy with benchmarking against global standards and competitors, identifying clear paths for improvement and growth.",
+      },
+    ],
   };
 
   const reasons = [
     {
-      "title": "Quick and Efficient ESG Assessment",
-      "description": "AI-powered rapid ESG assessment for maximum efficiency and deep insights.",
-      "icon": <MdCelebration />,
-      "url": "#"
+      title: "Quick and Efficient ESG Assessment",
+      description:
+        "AI-powered rapid ESG assessment for maximum efficiency and deep insights.",
+      icon: <MdCelebration />,
+      url: "#",
     },
     {
-      "title": "Comprehensive ESG Coverage",
-      "description": "Comprehensive coverage of the most common ESG factors from leading frameworks.",
-      "icon": <MdOutlineReport />,
-      "url": "#"
+      title: "Comprehensive ESG Coverage",
+      description:
+        "Comprehensive coverage of the most common ESG factors from leading frameworks.",
+      icon: <MdOutlineReport />,
+      url: "#",
     },
     {
-      "title": "Customized for Your Business",
-      "description": "Tailored assessment criteria and tools matched to your business size.",
-      "icon": <FaLevelUpAlt />,
-      "url": "#"
+      title: "Customized for Your Business",
+      description:
+        "Tailored assessment criteria and tools matched to your business size.",
+      icon: <FaLevelUpAlt />,
+      url: "#",
     },
     {
-      "title": "Continuous Innovation",
-      "description": "Continuous innovation and updates based on the latest regulations and research.",
-      "icon": <PiStrategy />,
-      "url": "#"
+      title: "Continuous Innovation",
+      description:
+        "Continuous innovation and updates based on the latest regulations and research.",
+      icon: <PiStrategy />,
+      url: "#",
     },
     {
-      "title": "Actionable Insights",
-      "description": "Actionable insights to target priority areas for improvement.",
-      "icon": <MdCelebration />,
-      "url": "#"
+      title: "Actionable Insights",
+      description:
+        "Actionable insights to target priority areas for improvement.",
+      icon: <MdCelebration />,
+      url: "#",
     },
     {
-      "title": "Expert-led Advice",
-      "description": "Expert-led advice and strategic roadmap for effective sustainability implementation.",
-      "icon": <MdCelebration />,
-      "url": "#"
+      title: "Expert-led Advice",
+      description:
+        "Expert-led advice and strategic roadmap for effective sustainability implementation.",
+      icon: <MdCelebration />,
+      url: "#",
     },
     {
-      "title": "Automated Mapping",
-      "description": "Automated mapping of the responses against multiple industry standards.",
-      "icon": <MdCelebration />,
-      "url": "#"
-    }
+      title: "Automated Mapping",
+      description:
+        "Automated mapping of the responses against multiple industry standards.",
+      icon: <MdCelebration />,
+      url: "#",
+    },
   ];
 
   const howToUse = [
     {
-      "title": "Complete the Online Questionnaire",
-      "description": "Start by filling out our comprehensive online questionnaire to assess your current ESG performance.",
-      "icon": (<div className="mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">1</div>),
-      "url": "#"
+      title: "Complete the Online Questionnaire",
+      description:
+        "Start by filling out our comprehensive online questionnaire to assess your current ESG performance.",
+      icon: (
+        <div className="mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
+          1
+        </div>
+      ),
+      url: "#",
     },
     {
-      "title": "Receive AI-powered Insights",
-      "description": "Our AI analyzes your responses to provide a detailed appraisal of your ESG program against global standards.",
-      "icon": (<div className="mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">2</div>),
-      "url": "#"
+      title: "Receive AI-powered Insights",
+      description:
+        "Our AI analyzes your responses to provide a detailed appraisal of your ESG program against global standards.",
+      icon: (
+        <div className="mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
+          2
+        </div>
+      ),
+      url: "#",
     },
     {
-      "title": "Develop Your Sustainability Roadmap",
-      "description": "Based on the analysis, receive a tailored roadmap with priority actions to enhance your sustainability initiatives.",
-      "icon": (<div className="mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">3</div>),
-      "url": "#"
-    }
+      title: "Develop Your Sustainability Roadmap",
+      description:
+        "Based on the analysis, receive a tailored roadmap with priority actions to enhance your sustainability initiatives.",
+      icon: (
+        <div className="mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
+          3
+        </div>
+      ),
+      url: "#",
+    },
   ];
 
   const CTA2Data = {
     title: "Start Your Sustainability Transformation Today",
     CTATitle: "Engage with LevelUp ESG Pulse",
-    CTAPath: "/contact-us"
+    CTAPath: "/contact-us",
   };
-  
 
   return (
-    <div className='flex justify-center items-center flex-col'>
+    <div className="flex justify-center items-center flex-col">
       {/* Start header ============================================= */}
       <Hero_2 {...heroData} />
       {/* End header ============================================= */}
@@ -126,54 +151,59 @@ const PulsePage = () => {
       <Futures />
       {/* End Futures ============================================ */}
 
-
       {/* Start why section ======================================== */}
       <div
-        style={{ backgroundImage: `url(${backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', }}
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
         className="w-full bg-[#F9FFF5] shadow-none"
       >
-        <section
-
-          className='why max-w-7xl mx-auto pt-28'>
-          <div className='px-[5%] w-full pb-20'>
-            <div className='flex flex-col gap-1'>
-              <div className='flex flex-col max-w-[90.25rem] gap-3'>
-                <h1 className='mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-[#002E70] md:text-5xl lg:text-6xl '>
+        <section className="why max-w-7xl mx-auto pt-28">
+          <div className="px-[5%] w-full pb-20">
+            <div className="flex flex-col gap-1">
+              <div className="flex flex-col max-w-[90.25rem] gap-3">
+                <h1 className="mb-4 text-4xl max-w-[1000px] font-extrabold tracking-tight leading-none text-[#002E70] md:text-5xl lg:text-6xl ">
                   Why Choose LevelUp ESG Pulse?
                 </h1>
-                <p className='mb-8 text-lg font-normal text-[#002E70]/80 lg:text-xl  '>
-
+                <p className="mb-8 text-lg font-normal text-[#002E70]/80 lg:text-xl  ">
                   LevelUp empowers businesses with affordable and innovative
-                  corporate sustainability solutions, expert-led advice, assurance,
-                  and a knowledge and resources hub.Our AI engines, service offerings, and data science simplify ESG execution, driving measurable impact.
+                  corporate sustainability solutions, expert-led advice,
+                  assurance, and a knowledge and resources hub.Our AI engines,
+                  service offerings, and data science simplify ESG execution,
+                  driving measurable impact.
                 </p>
-                <div className=''>
+                <div className="">
                   {" "}
-                  <div className='w-full border-[#02c2ab]  border-b-2     mb-10'></div>
+                  <div className="w-full border-[#02c2ab]  border-b-2     mb-10"></div>
                 </div>
               </div>
 
-              <div className='flex flex-wrap gap-4 justify-center'>
+              <div className="flex flex-wrap gap-4 justify-center">
                 {reasons.map((reason, index) => (
                   <a
                     href={reason.url}
                     key={index}
-
-                    className='reason-link max-w-[500px] border-2 border-gray-200/80 backdrop-blur-lg   transition duration-500 hover:bg-gray-200 transform hover:scale-95 rounded-2xl	p-6'>
+                    className="reason-link max-w-[500px] border-2 border-gray-200/80 backdrop-blur-lg   transition duration-500 hover:bg-gray-200 transform hover:scale-95 rounded-2xl	p-6"
+                  >
                     <div className="flex gap-2">
-                      <div className='reason-item mb-3 flex items-center justify-center rounded-md bg-[#02c2ab] aspect-square w-[40px] h-[40px] '>
+                      <div className="reason-item mb-3 flex items-center justify-center rounded-md bg-[#02c2ab] aspect-square w-[40px] h-[40px] ">
                         {reason.icon}
                       </div>
                       <div className="grid place-items-center">
                         {" "}
-                        <h3 className='mb-4 text-2xl max-w-[1000px] font-extrabold tracking-tight leading-6 text-[#002E70] md:text-2xl lg:text-3xl '>{reason.title}</h3>
+                        <h3 className="mb-4 text-2xl max-w-[1000px] font-extrabold tracking-tight leading-6 text-[#002E70] md:text-2xl lg:text-3xl ">
+                          {reason.title}
+                        </h3>
                       </div>
                     </div>
                     <div className="min-h-[100px] grid place-items-center">
-                      <p className="mb-8 text-lg font-normal text-[#002E70] lg:text-xl ">{reason.description}</p>
-
+                      <p className="mb-8 text-lg font-normal text-[#002E70] lg:text-xl ">
+                        {reason.description}
+                      </p>
                     </div>
-
                   </a>
                 ))}
               </div>
@@ -192,7 +222,7 @@ const PulsePage = () => {
       {/* End benifit section ==================================== */}
 
       {/* Start about section ====================================== */}
-      <div className='bg-[#121212] py-12 md:py-20 text-white w-full flex flex-col justify-center items-center'>
+      <div className=" py-12 md:py-20 text-[#002e70] w-full flex flex-col justify-center items-center">
         <div className="w-full flex -mt-5 justify-center ">
           <img src={DS} alt="Hero Image" className="w-full max-w-96" />
         </div>
@@ -200,7 +230,7 @@ const PulsePage = () => {
         <CardData
           title="How to use"
           reasons={howToUse}
-          Sectiondescription='Discover how our streamlined process equips you with the insights and actions needed to elevate your environmental, social, and governance (ESG) performance in just three simple steps.'
+          Sectiondescription="Discover how our streamlined process equips you with the insights and actions needed to elevate your environmental, social, and governance (ESG) performance in just three simple steps."
         />
       </div>
       {/* End about section ====================================== */}
@@ -209,6 +239,6 @@ const PulsePage = () => {
       <CTA1 />
     </div>
   );
-}
+};
 
 export default PulsePage;
