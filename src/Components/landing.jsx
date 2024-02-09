bf11404b2bc2a693dd1b66417d8fc19b17c3804b
devbn3li <fudwhc@gmail.com>
2024-02-10 01:58:13 +0200

    interactive

--- src/Components/landing.jsx
+++ src/Components/landing.jsx
@@ -1,39 +1,106 @@
 import React from "react";
-import img from "../../public/assets/Premasset/488f3192-902f-46cd-bcb7-6a50c298d1f5.png";
-import img2 from "../../public/assets/Premasset/0da8ef77-4960-4cfa-96fb-65136217e617.png";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import img3 from "../../public/assets/Premasset/dashhero2.png";
 import PrimaryButton from "./Buttons/PrimaryButton";
 
 const LandingSection = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  React.useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  // Animation variants
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+
+  const hoverImageAnimation = {
+    scale: 1.05,
+    transition: {
+      duration: 0.7,
+    },
+  };
+
+  const simpleFloatAnimation = {
+    y: ["-20px", "25px"],
+    transition: {
+      repeat: Infinity,
+      repeatType: "reverse",
+      duration: 4,
+      ease: "easeInOut",
+    },
+  };
+
   return (
-    <section className="my-28 max-md:mt-40">
-      <div className="container mx-auto px-[5%]">
+    <section className="my-28 max-md:mt-40" ref={ref}>
+      <motion.div
+        className="container mx-auto px-[5%]"
+        variants={containerVariants}
+        initial="hidden"
+        animate={controls}
+      >
         <div className="flex flex-wrap justify-between items-center">
-          <div className="w-full md:w-1/2 text-left">
+          <motion.div
+            className="w-full md:w-1/2 text-left"
+            variants={itemVariants}
+          >
             <h2 className="text-6xl font-extrabold text-[#002e70] max-md:text-center">
               LevelUp for Better Business & Planet.
             </h2>
-            <p className="my-12 text-[#002e70] max-md:text-center text-2xl">
+            <motion.p
+              className="my-12 text-[#002e70] max-md:text-center text-2xl"
+              variants={itemVariants}
+            >
               Welcome to LevelUp, where we combine sustainability technology,
               science, and human expertise to make a meaningful impact.
-            </p>
+            </motion.p>
             <div className="flex gap-6 max-md:flex-col">
-              <PrimaryButton path="/contact" title="About Us" />
               <PrimaryButton
-                path="/contact"
-                classes="bg-[#E6E6E5] border-none !text-[#002e70] font-bold"
+                path="/about"
+                title="About Us"
+                additionalClasses="hover:bg-blue-600 transition duration-300"
+              />
+              <PrimaryButton
+                path="/impact"
                 title="Make an Impact, Together"
+                additionalClasses="hover:bg-gray-300 transition duration-300"
               />
             </div>
-          </div>
-          <div className="w-full md:w-1/2 mt-4 md:mt-0 max-md:hidden">
-            <div className="flex justify-center  p-4 rounded-lg my-6">
-              <img src={img3} className="w-[80%]" alt="Landing section" />
+          </motion.div>
+          <motion.div
+            className="w-full md:w-1/2 mt-4 md:mt-0 max-md:hidden"
+            variants={itemVariants}
+            whileHover={hoverImageAnimation}
+          >
+            <div className="flex justify-center p-4 rounded-lg my-6">
+              <motion.img
+                src={img3}
+                className="w-[80%]"
+                alt="Landing section"
+                animate={simpleFloatAnimation}
+              />
             </div>
-
-          </div>
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
