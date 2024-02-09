070fa2233c3cee6571ba87644bda8ae2efc8c1aa
sondos <so_saad@yahoo.com>
2024-02-09 14:30:58 +0200

    fix

--- src/Components/Qoute.jsx
+++ src/Components/Qoute.jsx
@@ -2,12 +2,12 @@ import personImagePath from "../../public/assets/images/DrAhmedShawky.png";
 
 const FounderNote = () => {
   return (
-    <div className="relative flex justify-center items-center my-8 px-4 md:px-[5%]">
+    <div className="relative flex justify-center items-center my-12 px-4 md:px-[5%]">
       <div className="relative  p-6 rounded-lg  max-w-6xl w-full ">
         {/* Text content container with custom borders */}
         <div className="relative before:content-['“'] before:absolute before:text-6xl before:text-[#4dcfb7] before:-top-8 md:before:-top-6 before:left-0 md:before:left-0 md:border-r-[6px] md:border-b-[6px] md:border-[#4dcfb7]">
-          <blockquote className="text-3xl md:max-w-[60%] font-bold text-[#002E70]">
-            <p className="mb-6">
+          <blockquote className="text-xl lg:text-3xl md:max-w-[60%] font-bold text-[#002E70]">
+            <p className="mb-6 ">
             At LevelUp, we aim to make sustainability achievable for organisations of all sizes. Our impact makers and AI-powered solutions provide the tools and knowledge to pave your unique sustainability journey.
 
 
