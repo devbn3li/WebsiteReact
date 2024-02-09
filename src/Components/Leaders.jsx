730b10a15e6e729a29d0de0488715c3cab1b8081
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-02-09 15:04:10 +0200

    Update team members' photos in Leaders.jsx

--- src/Components/Leaders.jsx
+++ src/Components/Leaders.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import Ahmed from "../../public/assets/images/Leaders/Untitled.png";
-import Prasad from "../../public/assets/images/Leaders/Untitled (1).png";
-import Don from "../../public/assets/images/Leaders/Untitled (4).png";
-import Ekta from "../../public/assets/images/Leaders/Untitled (3).png";
-import Hamdy from "../../public/assets/images/Leaders/Untitled (2).png";
+import Ahmed from "../../public/assets/images/Leaders/ahmed.png";
+import Prasad from "../../public/assets/images/Leaders/prasad.png";
+import Don from "../../public/assets/images/Leaders/don.png";
+import Ekta from "../../public/assets/images/Leaders/ekta.png";
+import Hamdy from "../../public/assets/images/Leaders/hamdy.png";
+import yana from "../../public/assets/images/Leaders/yana.png";
 
 export default function TeamMembers() {
   const dataTeam = [
     { id: 1, photo: Ahmed, userName: "Ahmed Shawky", },
     { id: 2, photo: Don, userName: "Don Liyanage",Class:"" },
     { id: 3, photo: Ekta, userName: "Ekta Soni" },
-    { id: 4, photo: Prasad, userName: "Prasad Bhalerao" },
+    { id: 6, photo: yana, userName: "Yana Moldovan" },
+     { id: 4, photo: Prasad, userName: "Prasad Bhalerao" },
     { id: 5, photo: Hamdy, userName: "Hamdy Saad" },
+   
   ];
   return (
-    <div className='flex justify-center '>
-      <section className='relative w-full max-w-[1400px] py-[4rem] sm:px-[3rem] '>
+    <div className='flex justify-center w-full '>
+      <section className='relative w-full  max-w-[1400px] py-[4rem] sm:px-[3rem] '>
         <div className='w-[100%] xl:w-[80%] mx-auto flex flex-col gap-6'>
           <h2 className='text-3xl  md:text-7xl font-bold  text-[2.625rem] mb-[.5rem] text-center '>
             <mark className=' px-4  rounded-3xl text-white bg-gradient-to-r from-[#1C4987] to-[#159D9B]'>
@@ -28,8 +31,9 @@ export default function TeamMembers() {
             Our Leadership Team guides our organization with integrity and innovation, driving us towards success through collaboration and visionary leadership.
           </p>
 
-         
-          <div className='relative mt-[5rem]   grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 justify-items-center gap-y-[6rem] md:gap-x-[6rem]  lg:gap-x-[6rem] '>
+       <div className="flex justify-center   w-full">
+          
+       <div className='relative mt-[5rem]  justify-center flex flex-wrap justify-items-center gap-y-[6rem] md:gap-x-[6rem]  lg:gap-x-[6rem] '>
             {dataTeam.map(({ id, photo, userName }) => (
               <div
                 className='relative rounder-bl-3xl rounder-br-3xl  hover:scale-105 ease-linear duration-500 cursor-pointer  rounded-3xl h-[250px] w-[250px] lg:h-[200px] lg:w-[200px] '
@@ -52,6 +56,7 @@ export default function TeamMembers() {
               </div>
             ))}
           </div>
+       </div>
         </div>
       </section>
     </div>
