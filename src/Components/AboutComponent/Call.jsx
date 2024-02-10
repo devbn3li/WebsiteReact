3a38e28a66a39234cd24395beb5dd43f0e4916dc
devbn3li <fudwhc@gmail.com>
2024-02-10 08:06:06 +0200

    enhancement

--- src/Components/AboutComponent/Call.jsx
+++ src/Components/AboutComponent/Call.jsx
@@ -5,7 +5,7 @@ const Qoute = ({ quoteText, buttonTitle, buttonPath }) => {
   return (
     <div className=" text-white pt-10 px-[5%] flex items-center justify-between">
       <div className="flex flex-col max-w-6xl p-6 md:p-20 items-center m-auto bg-gradient-to-r from-[#112b65] to-[#02c2ab] rounded-3xl">
-        <p className="text-xl md:text-xl text-center font-medium md:font-semibold mb-4 md:mb-12">
+        <p className="md:text-xl text-center md:font-semibold mb-4 md:mb-12">
           {quoteText}
         </p>
         <PrimaryButton title={buttonTitle} path={buttonPath} />
