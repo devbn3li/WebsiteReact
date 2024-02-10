cf4a28f2b0d88705d595fa8611beef64e7206abb
devbn3li <fudwhc@gmail.com>
2024-02-10 05:37:20 +0200

    Enhancement

--- src/Components/landing.jsx
+++ src/Components/landing.jsx
@@ -14,18 +14,18 @@ const LandingSection = () => {
 
   return (
     <section className="my-18">
-      <div className="container pt-32 mx-auto px-[5%] h-[800px] overflow-hidden bg-gradient-to-b from-[#112b65] to-[#02c2ab] rounded-b-[40px]">
+      <div className="container pt-32 pb-20 mx-auto px-[5%] md:h-[800px] overflow-hidden bg-gradient-to-b from-[#112b65] to-[#02c2ab] rounded-b-[40px]">
         <div className="flex flex-col justify-center items-center text-center">
           <h2
-            className="text-6xl md:w-[50%] font-extrabold text-white"
+            className="text-6xl max-md:text-4xl md:w-[50%] font-extrabold text-white"
             data-aos="fade-down"
             data-aos-duration="1000"
           >
-            LevelUp for <span className="text-[#40cdbc]">Better</span> Business
+            LevelUp for <span className="text-[#40cdbc] ease-in duration-300 hover:text-white">Better</span> Business
             & Planet.
           </h2>
           <p
-            className="my-12 md:w-[70%] text-white text-2xl"
+            className="my-12 md:w-[70%] text-white text-xl md:text-2xl"
             data-aos="fade-up"
             data-aos-duration="1200"
           >
@@ -33,27 +33,27 @@ const LandingSection = () => {
             science, and human expertise to make a meaningful impact.
           </p>
           <div
-            className="flex flex-wrap gap-6 justify-center max-md:w-full"
+            className="relative z-10 flex flex-wrap gap-6 justify-center max-md:flex-col-reverse"
             data-aos="fade-up"
             data-aos-duration="1400"
           >
             <div data-aos="flip-left" data-aos-delay="150">
-              <PrimaryButton path="/contact" classes="font-bold max-md:w-full" title="About Us" />
+              <PrimaryButton path="/contact" classes="font-bold hover:bg-white hover:!text-[#02c2ab] border-none max-md:w-full" title="About Us" />
             </div>
             <div data-aos="flip-right" data-aos-delay="300">
               <PrimaryButton
                 path="/contact"
-                classes="bg-[#E6E6E5] border-none !text-[#002e70] font-bold"
+                classes="bg-[#E6E6E5] border-none !text-[#002e70] font-bold hover:bg-[#002e70] hover:!text-white max-md:w-full"
                 title="Make an Impact, Together"
               />
             </div>
           </div>
           <div
-            className="w-full flex justify-center p-4 rounded-lg my-6 max-md:hidden"
+            className=" w-full flex justify-center p-4 rounded-lg my-6 max-md:hidden"
             data-aos="zoom-in-up"
             data-aos-duration="1600"
           >
-            <img src={img3} className="w-[80%]" alt="Landing section" />
+            <img src={img3} className="absolute -z-10 w-[80%] ease-in duration-500 hover:-mt-52" alt="Landing section" />
           </div>
         </div>
       </div>
