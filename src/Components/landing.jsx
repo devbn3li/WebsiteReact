bf8899f141e670f2fb42ad5a13c04e6e45f45b65
devbn3li <fudwhc@gmail.com>
2024-02-10 05:07:59 +0200

    landing page changes

--- src/Components/landing.jsx
+++ src/Components/landing.jsx
@@ -1,106 +1,62 @@
-import React from "react";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import img3 from "../../public/assets/Premasset/dashhero2.png";
+import React, { useEffect } from "react";
+import img3 from "../../public/assets/Dash/2.png";
 import PrimaryButton from "./Buttons/PrimaryButton";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS styles
 
 const LandingSection = () => {
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
-
-  React.useEffect(() => {
-    if (inView) {
-      controls.start("visible");
-    } else {
-      controls.start("hidden");
-    }
-  }, [controls, inView]);
-
-  // Animation variants
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.3,
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: { y: 20, opacity: 0 },
-    visible: { y: 0, opacity: 1 },
-  };
-
-  const hoverImageAnimation = {
-    scale: 1.05,
-    transition: {
-      duration: 0.7,
-    },
-  };
-
-  const simpleFloatAnimation = {
-    y: ["-20px", "25px"],
-    transition: {
-      repeat: Infinity,
-      repeatType: "reverse",
-      duration: 4,
-      ease: "easeInOut",
-    },
-  };
+  useEffect(() => {
+    AOS.init({
+      once: true, // Whether animation should happen only once - while scrolling down
+      mirror: false, // Whether elements should animate out while scrolling past them
+    });
+  }, []);
 
   return (
-    <section className="my-28 max-md:mt-40" ref={ref}>
-      <motion.div
-        className="container mx-auto px-[5%]"
-        variants={containerVariants}
-        initial="hidden"
-        animate={controls}
-      >
-        <div className="flex flex-wrap justify-between items-center">
-          <motion.div
-            className="w-full md:w-1/2 text-left"
-            variants={itemVariants}
+    <section className="my-18">
+      <div className="container pt-32 mx-auto px-[5%] h-[800px] overflow-hidden bg-gradient-to-b from-[#112b65] to-[#02c2ab] rounded-b-[40px]">
+        <div className="flex flex-col justify-center items-center text-center">
+          <h2
+            className="text-6xl md:w-[50%] font-extrabold text-white"
+            data-aos="fade-down"
+            data-aos-duration="1000"
           >
-            <h2 className="text-6xl font-extrabold text-[#002e70] max-md:text-center">
-              LevelUp for Better Business & Planet.
-            </h2>
-            <motion.p
-              className="my-12 text-[#002e70] max-md:text-center text-2xl"
-              variants={itemVariants}
-            >
-              Welcome to LevelUp, where we combine sustainability technology,
-              science, and human expertise to make a meaningful impact.
-            </motion.p>
-            <div className="flex gap-6 max-md:flex-col">
-              <PrimaryButton
-                path="/about"
-                title="About Us"
-                Classes="hover:bg-blue-600 transition duration-300 !font-bold"
-              />
+            LevelUp for <span className="text-[#40cdbc]">Better</span> Business
+            & Planet.
+          </h2>
+          <p
+            className="my-12 md:w-[70%] text-white text-2xl"
+            data-aos="fade-up"
+            data-aos-duration="1200"
+          >
+            Welcome to LevelUp, where we combine sustainability technology,
+            science, and human expertise to make a meaningful impact.
+          </p>
+          <div
+            className="flex flex-wrap gap-6 justify-center max-md:w-full"
+            data-aos="fade-up"
+            data-aos-duration="1400"
+          >
+            <div data-aos="flip-left" data-aos-delay="150">
+              <PrimaryButton path="/contact" classes="font-bold max-md:w-full" title="About Us" />
+            </div>
+            <div data-aos="flip-right" data-aos-delay="300">
               <PrimaryButton
                 path="/contact"
-                title="Make an Impact, Together"
                 classes="bg-[#E6E6E5] border-none !text-[#002e70] font-bold"
+                title="Make an Impact, Together"
               />
             </div>
-          </motion.div>
-          <motion.div
-            className="w-full md:w-1/2 mt-4 md:mt-0 max-md:hidden"
-            variants={itemVariants}
-            whileHover={hoverImageAnimation}
+          </div>
+          <div
+            className="w-full flex justify-center p-4 rounded-lg my-6 max-md:hidden"
+            data-aos="zoom-in-up"
+            data-aos-duration="1600"
           >
-            <div className="flex justify-center p-4 rounded-lg my-6">
-              <motion.img
-                src={img3}
-                className="w-[80%]"
-                alt="Landing section"
-                animate={simpleFloatAnimation}
-              />
-            </div>
-          </motion.div>
+            <img src={img3} className="w-[80%]" alt="Landing section" />
+          </div>
         </div>
-      </motion.div>
+      </div>
     </section>
   );
 };
