9a85fa30e2379230a52e6683dc53ccd6ce793835
DarkCode662432 <ff9371668@gmail.com>
2024-02-11 05:35:11 +0200

    [enhancement]

--- src/Components/Benefits/Benefits.jsx
+++ src/Components/Benefits/Benefits.jsx
@@ -1,16 +1,28 @@
 // Import your external library dependencies and component dependencies
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import BenefitsCard from './BenefitsCard';
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS styles
+
 
 // Benefits component that renders list of benefits in a section
 const Benefits = ({ title, description, benefits }) => {
+  useEffect(() => {
+    AOS.init({
+      once: true, // Whether animation should happen only once - while scrolling down
+      mirror: false, // Whether elements should animate out while scrolling past them
+    });
+  }, []);
 
   // Render the section with a title, description and list of BenefitCard components mapped from benefits array
   return (
    <div className='w-full justify-center flex'>
      <section className='w-full max-w-[1400px] pt-24 px-[5%] gap-3'>
-      <div className='max-w-[90.25rem] mb-10'>
+      <div className='max-w-[90.25rem] mb-10'
+      data-aos="fade-down"
+      data-aos-duration="1600"
+      >
         <h1 className="mb-4 text-4xl max-w-[1200px] font-extrabold tracking-tight leading-none text-[#002E70] md:text-5xl lg:text-6xl ">
           {title}</h1>
         <p className="mb-8 text-lg font-normal text-[#002E70]/80 lg:text-xl  ">
