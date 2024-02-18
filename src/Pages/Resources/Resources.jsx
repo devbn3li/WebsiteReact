f69b60c7586805441d9f00e545f4082ad0beef76
devbn3li <fudwhc@gmail.com>
2024-02-18 14:02:47 +0200

    Hero

--- src/Pages/Resources/Resources.jsx
+++ src/Pages/Resources/Resources.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import DynamicLandingHero from "../../Components/DynamicLandingHero";
 
 function Resources() {
   return (
     <div>
-
+      <DynamicLandingHero
+        title="Resources"
+        subtitle="At LevelUp, we&apos;re a passionate team of sustainability experts and scientists with a mission to make corporate sustainability and  ESG practices achievable for businesses of all sizes. We achieve this by offering innovative and affordable ESG solutions alongside expert advice."
+        button1={{ path: "/contact", label: "Contact Us" }}
+        button2={{ path: "/contact", label: "Make an Impact, Together" }}
+      />
     </div>
   );
 }
