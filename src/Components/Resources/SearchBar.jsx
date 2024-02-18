a521e64b08ac4fc7a66e4c95ff6813b5e71f7e32
devbn3li <fudwhc@gmail.com>
2024-02-18 21:05:42 +0200

    enhancement

--- src/Components/Resources/SearchBar.jsx
+++ src/Components/Resources/SearchBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function SearchBar() {
   return (
-    <div className=" bg-gradient-to-r from-[#1C4987] to-[#159D9B] rounded-3xl py-8 mt-20 px-[5%] md:h-[1000px] ">
+    <div className=" bg-gradient-to-r from-[#1C4987] to-[#159D9B] rounded-3xl py-8 md:mt-20 px-[5%] md:h-[1000px] ">
       <div className="relative flex items-center md:mb-10">
         <input
-          type="text"
+          type="search"
           placeholder="Search"
           className="w-[95%] mx-auto py-2 px-4 rounded-[5rem] outline-none text-black text-xl"
         />
