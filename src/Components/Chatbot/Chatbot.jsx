06807a6c0ffc4ba89e8f5cac24ae3cdf1985bac4
Hamdy Saad <hamdysaad.biz@gmail.com>
2024-02-24 17:27:51 +0200

    Update Chatbot UI and messaging

--- src/Components/Chatbot/Chatbot.jsx
+++ src/Components/Chatbot/Chatbot.jsx
@@ -47,18 +47,18 @@ function Chatbot() {
     <>
       <section className="bg-gradient-to-r from-[#002e70] to-[#02c1aa] w-full rounded-2xl py-[5rem] max-sm:py-8">
         <div className="relative w-[90%] md:w-[80%] mx-auto flex justify-center items-center flex-col py-[4rem] pt-[6rem] px-2rem text-white">
-          <h1 className="text-2xl md:text-5xl max-w-[700px] text-center md:font-bold tracking-wider mb-10">
-          Unveiling Standards: A Guide by LevelUp IA
+          <h1 className="text-2xl md:text-5xl max-w-[700px] text-center md:font-bold tracking-wider mb-3">
+         The 2024 Global Internal Audit Standards
                     </h1>
           <p className="my-6 mt-4 md:w-[70%] text-center text-white text-xl leading-[1.7] aos-init aos-animate">
-          my name is LevelUp IA. I'm an AI assistant created by LevelUp to be helpful, harmless, and honest. Feel free to ask me any questions you may have about the 2024 Global Internal Audit Standards. I'll do my best to provide accurate and useful information to support your understanding of these standards.
+          👋 I am LevelUp IA, an AI assistant created by LevelUp to be helpful, harmless, and honest. Feel free to ask me any questions you may have about the 2024 Global Internal Audit Standards. I'll do my best to provide accurate and useful information to support your understanding of these standards.
           </p>
-          <form onSubmit={handleFormSubmit} className="relative flex items-center mb-10">
+          <form onSubmit={handleFormSubmit} className="relative  flex items-center mb-10">
             <input
               ref={inputRef}
               type="text"
               placeholder="Ask Me!"
-              className="input-placeholder max-[400px]:w-[300px] w-[300px] sm:w-[400px] md:w-[500px] py-3 px-4 rounded-[5rem] outline-none text-white text-xl bg-white/30 backdrop-blur-lg border border-gray-200/50"
+              className="input-placeholder shadow-2xl max-[400px]:w-[300px] w-[300px] sm:w-[400px] md:w-[500px] py-3 px-4 rounded-[5rem] outline-none text-white text-xl bg-white/30 backdrop-blur-lg border-2 border-gray-200/50"
               value={inputText}
               onChange={handleInputChange}
             />
@@ -89,8 +89,8 @@ function Chatbot() {
               </>
             )}
           </div>
-          <div className="mt-4 px-2 text-center text-white md:w-[60%]">
-            Please note: The information provided by the chatbot may not always be accurate. Use it as a guide and verify details independently.
+          <div className="mt-4 px-2 opacity-55 text-center text-xs text-white md:w-[60%]">
+          The information provided by the chatbot may not always be accurate. Use it as a guide and verify details independently.
           </div>
         </div>
       </section>
