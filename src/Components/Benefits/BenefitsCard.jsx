3aa190fca95873d50f670a6f0e121a2aab622770
devbn3li <fudwhc@gmail.com>
2024-02-28 15:47:34 +0200

    enhancement

--- src/Components/Benefits/BenefitsCard.jsx
+++ src/Components/Benefits/BenefitsCard.jsx
@@ -21,7 +21,7 @@ const BenefitsCard = ({ icon, title, description, imageUrl, Class }) => {
     <div
       data-aos="zoom-in-up"
       data-aos-duration="1600"
-      className='group flex flex-col  border-t  border-[#02c2ab] pt-6 gap-6 max-[996px]:flex-row max-[479px]:flex-col'>
+      className='group flex flex-col  border-t justify-between border-[#02c2ab] pt-6 gap-6 max-[996px]:flex-row max-[479px]:flex-col'>
       <div className='group-hover:pl-2 duration-500 flex flex-col max-[991px]:w-11/12'>
         <span className='flex justify-center items-center bg-[#02c2ab] hover:rotate-6 duration-700 cursor-pointer hover:scale-110 mb-3 text-[#002E70] aspect-square max-w-10 box-border p-2 rounded-lg'>
           {icon}
@@ -30,8 +30,8 @@ const BenefitsCard = ({ icon, title, description, imageUrl, Class }) => {
           {title}</h1>
         <p className="mb-8 text-lg font-normal text-gray-500 text-[#002E70]/80  ">{description}</p>
       </div>
-      <div className={`${!imageUrl && 'hidden'} min-[479px]:mt-[7rem] md:mt-0  hover:scale-105 duration-700 cursor-pointer object-cover w-[80%] h-[250px] rounded-2xl overflow-hidden`}>
-        <img src={imageUrl} class={Class} alt={`Benefit: ${title}`} className='object-cover  h-full w-full' />
+      <div className={`${!imageUrl && 'hidden'} min-[479px]:mt-[7rem] md:mt-0 hover:scale-105 flex justify-center duration-700 cursor-pointer object-cover  h-[250px] rounded-2xl overflow-hidden`}>
+        <img src={imageUrl} class={Class} alt={`Benefit: ${title}`} className='object-cover h-full w-full' />
       </div>
     </div>
   );
