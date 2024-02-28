b2844d1d6ad7b26c54cef684cd493a8173f42836
sondos <so_saad@yahoo.com>
2024-02-28 23:30:41 +0200

    Enhancing

--- src/Components/IAProgram.jsx
+++ src/Components/IAProgram.jsx
@@ -1,54 +1,10 @@
 import React from 'react'
+import plan from '../../public/assets/plan.png'
+import report from '../../public/assets/report.png'
+import growth from '../../public/assets/growth.png'
 
 function IAProgram() {
-  const TitleSection = ["governance" , "planning" , "Execution & reporting"]  ;
-  const Data =[
-    {   
-        id:1,
-        subtitle:"Internal Audit Strategy",
-        description:"Document outlining the goals, scope, and approach of the Internal Audit (IA) program."
-    },
-    {   
-        id:2,
-        subtitle:"Risk Assessment",
-        description:"Identifying and evaluating ESG risks and prioritize audit activities to cover critical risks."
-    },
-    {   
-        id:3,
-        subtitle:"Audit Testing and Sampling",
-        description:"Procedures for conducting audit testing and sampling."
-    },
-    {   
-        id:4,
-        subtitle:"Internal Audit Manual",
-        description:"Document providing guidance and procedures for conducting ESG audits."
-    },
-    {   
-        id:5,
-        subtitle:"Planning and Execution",
-        description:"Document providing guidance and procedures for conducting ESG audits."
-    },
-    {   
-        id:6,
-        subtitle:"Findings and Recommendations",
-        description:"Document of audit results and suggesting actions to address identified issues."
-    },
-    {   
-        id:7,
-        subtitle:"Training and Professional Development Programs",
-        description:"Audit team to enhance their knowledge around ESG Topics."
-    },
-    {   
-        id:8,
-        subtitle:" Audit Checklist",
-        description:"Audit checklist for assessing key areas, evaluating ESG criteria, and reviewing pertinent documentation during the audit process."
-    },
-    {   
-        id:9,
-        subtitle:"Follow-ip and Corrective",
-        description:"Track and monitor the implementation of corrective actions in response to audit findings."
-    },
-  ]
+  
 const GOVERNANCE = [
     {   
         id:1,
@@ -104,63 +60,67 @@ const EXECUTION = [
   return (
     <section className='py-[4rem]'>
         <div className='w-[80%] mx-auto mb-5'>
-            <h1 className='uppercase text-4xl font-semibold'>key components of IA Audit program</h1>   
+            <h1 className='uppercase text-2xl md:text-4xl font-semibold text-center'>key components of IA Audit program</h1>   
         </div>
         <div className='w-full h-[1px] bg-[#eee] mb-5'></div>
         <div className='w-[80%] mx-auto mt-10'>
                 <div>
-                    <div className='flex flex-wrap md:flex-nowrap gap-x-10 gap-y-5 '>
-                        
+                    <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-5 '> 
                         <div className=''>
-                        <h2 className='text-[#002E70] uppercase text-2xl font-[600] tracking-[.5px] mb-5'>governance</h2>
-                        {
-                            GOVERNANCE.map((item, id)=>{
-                                return(                            
-                                        <div className='mb-5' key={id}>
-                                            
-                                            <h2 className='font-bold tracking-[.5px] text-lg leading-[1.5]'>{item.subtitle}</h2>
-                                            <div className='w-[200px] h-[2px] bg-[#02BEA9] mt-2'></div>
-                                            <p className='tracking-[.5px] leading-[1.5] mt-3 text-[#002E70]'>{item.description}</p>
-                                        </div>                         
-                                    )
+                            <div className='w-[90%] h-[280px] xl:h-[330px]'>
+                                    <img src={growth}  className='w-full h-full'/>
+                            </div>
+                            <h2 className='text-[#002E70] uppercase text-2xl font-[600] tracking-[.5px] mb-5 mt-10 lg:mt-0'>governance</h2>
+                            {
+                                GOVERNANCE.map((item, id)=>{
+                                    return(                            
+                                            <div className='mb-5 h-[130px] xl:w-[300px]' key={id}>
+                                                <h2 className='font-bold tracking-[.5px] text-lg leading-[1.5]'>{item.subtitle}</h2>
+                                                <div className='w-[200px] h-[2px] bg-[#02BEA9] mt-2'></div>
+                                                <p className='tracking-[.5px] leading-[1.5] mt-3 text-[#002E70]'>{item.description}</p>
+                                            </div>                         
+                                        )
+                                }
+                                )
                             }
-                            )
-                        }
-                    
                         </div>
                         <div className=''>
-                        <h2 className='text-[#002E70] uppercase text-2xl font-[600] tracking-[.5px] mb-5'>planning</h2>
-                        {
-                            PLANNING.map((item, id)=>{
-                                return(                            
-                                        <div className='mb-5' key={id}>
-                                            
-                                            <h2 className='font-bold tracking-[.5px] text-lg leading-[1.5]'>{item.subtitle}</h2>
-                                            <div className='w-[200px] h-[2px] bg-[#02BEA9] mt-2'></div>
-                                            <p className='tracking-[.5px] leading-[1.5] mt-3 text-[#002E70]'>{item.description}</p>
-                                        </div>                         
-                                    )
+                            <div className='w-[90%] h-[280px] xl:h-[330px]'>
+                                <img src={plan} className='w-full h-full'/>
+                            </div>
+                            <h2 className='text-[#002E70] uppercase text-2xl font-[600] tracking-[.5px] mb-5 mt-10 lg:mt-0'>planning</h2>
+                            {
+                                PLANNING.map((item, id)=>{
+                                    return(                            
+                                            <div className='mb-5 h-[130px] xl:w-[300px]' key={id}>
+                                                
+                                                <h2 className='font-bold tracking-[.5px] text-lg leading-[1.5]'>{item.subtitle}</h2>
+                                                <div className='w-[200px] h-[2px] bg-[#02BEA9] mt-2'></div>
+                                                <p className='tracking-[.5px] leading-[1.5] mt-3 text-[#002E70]'>{item.description}</p>
+                                            </div>                         
+                                        )
+                                }
+                                )
                             }
-                            )
-                        }
-                    
                         </div>
                         <div className=''>
-                        <h2 className='text-[#002E70] uppercase text-2xl font-[600] tracking-[.5px] mb-5'>Execution & reporting</h2>
-                        {
-                            EXECUTION.map((item, id)=>{
-                                return(                            
-                                        <div className='mb-5' key={id}>
-                                            
-                                            <h2 className='font-bold tracking-[.5px] text-lg leading-[1.5]'>{item.subtitle}</h2>
-                                            <div className='w-[200px] h-[2px] bg-[#02BEA9] mt-2'></div>
-                                            <p className='tracking-[.5px] leading-[1.5] mt-3 text-[#002E70]'>{item.description}</p>
-                                        </div>                         
-                                    )
+                            <div className='w-[90%] h-[280px] xl:h-[330px]'>
+                                <img src={report} className='w-full h-full'/>
+                            </div>
+                            <h2 className='text-[#002E70] uppercase text-2xl font-[600] tracking-[.5px] mb-5 mt-10 lg:mt-0'>Execution & reporting</h2>
+                            {
+                                EXECUTION.map((item, id)=>{
+                                    return(                            
+                                            <div className='mb-5 h-[130px] xl:w-[300px]' key={id}>
+                                                
+                                                <h2 className='font-bold tracking-[.5px] text-lg leading-[1.5]'>{item.subtitle}</h2>
+                                                <div className='w-[200px] h-[2px] bg-[#02BEA9] mt-2'></div>
+                                                <p className='tracking-[.5px] leading-[1.5] mt-3 text-[#002E70]'>{item.description}</p>
+                                            </div>                         
+                                        )
+                                }
+                                )
                             }
-                            )
-                        }
-                    
                         </div>
                     </div>
                     
