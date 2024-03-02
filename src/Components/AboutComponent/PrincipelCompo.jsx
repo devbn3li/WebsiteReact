3626d61d046a5284b05430c9659b198056f26f5b
devbn3li <fudwhc@gmail.com>
2024-03-02 12:00:19 +0200

    max width 1400px

--- src/Components/AboutComponent/PrincipelCompo.jsx
+++ src/Components/AboutComponent/PrincipelCompo.jsx
@@ -3,7 +3,7 @@ import PrimaryButton from "../Buttons/PrimaryButton";
 
 function PrincipelCompo() {
   return (
-    <section className="py-12 mb-28 bg-gradient-to-r from-[#1C4987] to-[#159D9B]">
+    <section className="py-12 mb-28 bg-gradient-to-r from-[#1C4987] to-[#159D9B] max-w-[1400px] mx-auto">
       <div className="max-w-[1000px] px-3 mx-auto ">
         <div className="text-center   ">
           <h2 className="font-semibold md:font-bold text-3xl md:text-5xl px-6 py-3 w-fit mx-auto mb-8 rounded-3xl text-white ">LevelUp Principles</h2>
