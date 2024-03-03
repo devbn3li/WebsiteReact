be08c758548ae52ddd8085719eeee1b8da310dea
devbn3li <fudwhc@gmail.com>
2024-03-03 11:33:30 +0200

    Blog slider

--- src/Pages/Blogs/BlogsDiscoveryPage.jsx
+++ src/Pages/Blogs/BlogsDiscoveryPage.jsx
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from "react";
-import BlogCard from "../../Components/BlogCard/BlogCard";
+import React from "react";
+import BlogHero from "../../Components/Blog/BlogHero";
+import BlogShow from "../../Components/Blog/BlogShow";
 
 const Blog = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  useEffect(() => {
-    fetch(process.env.BLOG_EXPLORE_API_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        setBlogs(Array.from(data));
-      })
-  }, []);
-
-  if (!blogs) return <div>Loading...</div>;
   return (
-    <div className="container mx-auto py-8 mt-20 px-[5%]">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {blogs.map((blog, index) => (
-          <BlogCard key={index} blog={{...blog}} />
-        ))}
-      </div>
+    <div>
+      <BlogHero />
+      <BlogShow />
     </div>
   );
 };
