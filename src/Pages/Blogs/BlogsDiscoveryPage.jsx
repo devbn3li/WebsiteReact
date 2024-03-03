7ad292cf792cb132a9248cc82ec50186a63b8ac8
devbn3li <fudwhc@gmail.com>
2024-03-03 12:23:34 +0200

    Show Blogs in the Discovery Page with more button

--- src/Pages/Blogs/BlogsDiscoveryPage.jsx
+++ src/Pages/Blogs/BlogsDiscoveryPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BlogHero from "../../Components/Blog/BlogHero";
+import BlogShow from "../../Components/Blog/BlogShow";
 
 const Blog = () => {
   return (
     <div>
       <BlogHero />
+      <BlogShow />
     </div>
   );
 };
