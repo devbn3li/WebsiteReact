e258b78abc4330f2409e388922dbb2033d59841f
devbn3li <fudwhc@gmail.com>
2024-03-03 18:18:28 +0200

    responsive design

--- src/Components/Blog/Call.jsx
+++ src/Components/Blog/Call.jsx
@@ -5,11 +5,11 @@ const Call = () => {
   return (
     <div>
       <section className="container px-[5%] mt-28 mx-auto">
-        <div className="flex gap-20 bg-[#002E70] py-12 mx-auto px-32 rounded-[14px]">
-          <div className="w-[470px]">
-            <img src={img} alt="" className="w-full h-[300px] object-cover" />
+        <div className="flex max-md:flex-col md:gap-20 bg-[#002E70] py-4 md:py-12 mx-auto px-[5%] md:px-32 rounded-[14px]">
+          <div className=" md:w-[470px]">
+            <img src={img} alt="CTA img" className="w-full h-[300px] object-cover " />
           </div>
-          <div className="flex flex-col items-center justify-between">
+          <div className="flex flex-col items-center justify-between ">
             <h2 className="text-3xl font-bold text-white mb-2">
               Green-Tok, a newsletter dedicated to climate green news
             </h2>
@@ -17,11 +17,11 @@ const Call = () => {
               We share green news once a month (or more if we find interesting
               things to tell you)
             </p>
-            <form className="w-full flex gap-10 content-start">
+            <form className="w-full flex max-md:flex-col gap-4 md:gap-10 content-start">
               <input
                 type="email"
                 placeholder="Your email address"
-                className="bg-white w-[300px] rounded-lg p-4 outline-none"
+                className="bg-white md:w-[300px] rounded-lg p-4 outline-none"
               />
               <input
                 type="submit"
