cbb26981fc91bfed0d4fb01cb27a0263dfc33bac
devbn3li <fudwhc@gmail.com>
2024-03-03 11:41:04 +0200

    fix

--- src/Pages/Blogs/BlogsDiscoveryPage.jsx
+++ src/Pages/Blogs/BlogsDiscoveryPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import BlogHero from "../../Components/Blog/BlogHero";
-import BlogShow from "../../Components/Blog/BlogShow";
 
 const Blog = () => {
   return (
     <div>
       <BlogHero />
-      <BlogShow />
     </div>
   );
 };
